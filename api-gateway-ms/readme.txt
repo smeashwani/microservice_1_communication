9c266d335d547d7406ab2e961ef3981ef9af4d28
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-07-10 13:52:47 +0530

    update

---
+++ api-gateway-ms/readme.txt
@@ -0,0 +1,2 @@
+http://localhost:9191/api/users/1
+http://localhost:9191/api/departments/1
---
+++ api-gateway-ms/src/main/java/com/training/apigatewayms/ApiGatewayMsApplication.java
@@ -0,0 +1,13 @@
+package com.training.apigatewayms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayMsApplication.class, args);
+	}
+
+}
