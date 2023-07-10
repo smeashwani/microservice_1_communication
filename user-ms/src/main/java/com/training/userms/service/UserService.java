26b0e845d3d4c8a193dda5ad72f76aacd8262755
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-07-10 13:19:53 +0530

    update

--- user-ms/src/main/java/com/training/userms/service/UserService.java
+++ user-ms/src/main/java/com/training/userms/service/UserService.java
@@ -1,8 +1,9 @@
 package com.training.userms.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import com.training.userms.config.APIClient;
 import com.training.userms.entity.UserEntity;
 import com.training.userms.model.DepartmentDto;
 import com.training.userms.model.ResponseDto;
@@ -16,7 +17,7 @@ import lombok.AllArgsConstructor;
 public class UserService {
 
 	private UserRepository userRepository;
-	private APIClient apiClient;
+	private RestTemplate restTemplate;
 
 	public UserDto save(UserEntity user) {
 		return mapToUser(userRepository.save(user));
@@ -27,7 +28,10 @@ public class UserService {
         UserEntity user = userRepository.findById(userId).get();
         UserDto userDto = mapToUser(user);
 
-        DepartmentDto departmentDto = apiClient.getDepartmentById(user.getDepartmentId());
+        ResponseEntity<DepartmentDto> responseEntity = restTemplate
+                .getForEntity("http://department-ms/api/departments/" + user.getDepartmentId(),
+                DepartmentDto.class);
+        DepartmentDto departmentDto = responseEntity.getBody();
         responseDto.setUser(userDto);
         responseDto.setDepartment(departmentDto);
 
