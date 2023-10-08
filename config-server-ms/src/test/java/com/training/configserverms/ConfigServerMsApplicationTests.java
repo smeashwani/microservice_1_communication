46a9fab1a2a9c1109c554a85f5c5ddd3fa81b3f8
Saxena, Ashwani <ashwani_saxena@optum.com>
2023-10-08 19:24:02 +0530

    Update

---
+++ config-server-ms/src/test/java/com/training/configserverms/ConfigServerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.configserverms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
